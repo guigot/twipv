75051ab526f3a1d482be14a6ff70a033db1fd321
guigot <gdiego@valvarium.fr>
2018-12-29 10:29:32 +0100

    Utilisation du paramètre save-position-on-quit de mpv plutot que positions.db

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,11 @@ use std::string::String;
 use curl::easy::{Handler, Easy2, List, WriteError};
 use serde_json::{Value};
 use std::str;
-use rusqlite::Connection;
 use cursive::Cursive;
 use cursive::align::HAlign;
 use cursive::theme::{Color, PaletteColor};
 use cursive::event::EventResult;
 use cursive::views::{Dialog,OnEventView,SelectView};
-
-const DB_POSITION: &str = "/home/exosta/programming/mpv_stream/positions.db";
 
 struct Collector(Vec<u8>);
 
@@ -45,33 +42,6 @@ fn retrieve_videos() -> String {
 
 }
 
-fn save_in_database(path: &Path, time_pos: i64) {
-
-    let connection = Connection::open(DB_POSITION).unwrap();
-
-    connection.execute("INSERT OR REPLACE INTO t_positions (url, time_pos)
-                       VALUES (
-                       ?1, ?2);", &[&path.to_str().unwrap(), &time_pos.to_string().as_str()]).unwrap();
-}
-
-fn get_time_pos_database(path: &Path) -> i64 {
-
-    let connection = Connection::open(DB_POSITION).unwrap();
-
-    let result = connection.query_row("SELECT time_pos FROM t_positions WHERE
-                         url = ?1;", &[&path.to_str().unwrap()],
-                         |row| row.get(0),);
-
-    match result {
-        Ok(time_pos) => { time_pos },
-        Err(ref error) => match error {
-            rusqlite::Error::QueryReturnedNoRows => { 0 },
-            _ => panic!("MISTAKE {:?}", error),
-        },
-    }
-
-}
-
 fn launch_video(video_path: &Path) {
     let mut mpv_builder = mpv::MpvHandlerBuilder::new().expect("Failed to init MPV builder");
     if video_path.is_file() || !video_path.is_file() {
@@ -79,6 +49,8 @@ fn launch_video(video_path: &Path) {
 
         // enable On Screen Controller (disabled with libmpv by default)
         mpv_builder.set_option("osc",true).unwrap();
+        mpv_builder.set_option("save-position-on-quit",true).unwrap();
+        mpv_builder.set_option("watch-later-directory","/home/exosta/.config/mpv/watch_later").unwrap();
         mpv_builder.set_option("input-default-bindings",true).unwrap();
         mpv_builder.set_option("input-vo-keyboard",true).unwrap();
 
@@ -93,28 +65,11 @@ fn launch_video(video_path: &Path) {
         // set speed to 100%, send parameter as a f64
         mpv.set_property("speed",1.0).unwrap();
 
-        let mut seeked = false;
-
         'main: loop {
             while let Some(event) = mpv.wait_event(0.0) {
                 // even if you don't do anything with the events, it is still necessary to empty
                 // the event loop
                 match event {
-                    // Shutdown will be triggered when the window is explicitely closed,
-                    // while Idle will be triggered when the queue will end
-                    mpv::Event::PlaybackRestart => {
-                        if !seeked {
-                            //let pos = get_time_pos(Path::new(video_path)).expect("error?");
-                            let pos = get_time_pos_database(Path::new(video_path));
-                            mpv.set_property("playback-time",pos).unwrap();
-                            seeked = true;
-                        }
-                    }
-                    mpv::Event::Pause => {
-                        let time_pos = mpv.get_property("time-pos").unwrap();
-                        save_in_database(Path::new(video_path), time_pos);
-                        //save_time_pos(Path::new(video_path), time_pos);
-                    }
                     mpv::Event::Shutdown | mpv::Event::Idle => {
                         break 'main;
                     }
@@ -123,29 +78,11 @@ fn launch_video(video_path: &Path) {
             }
         }
     } 
-    else {
-        /*
-        println!("A file is required; {} is not a valid file",
-                 video_path.to_str().unwrap());
-                 */
-    }
 }
 
 
-/*
-fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() < 2 {
-        retrieve_videos(-1);
-    } else {
-        retrieve_videos(args[1].parse::<i32>().unwrap());
-    }
-}*/
-
 fn callback_video(_siv: &mut Cursive, url: &str) {
-
     launch_video(Path::new(url));
-
 }
 
 fn main() {
@@ -181,10 +118,10 @@ fn main() {
     }
 
     let select_view = OnEventView::new(select_view)
-        .on_pre_event_inner('k', |s| {
+        .on_pre_event_inner('k', |s, _| {
             s.select_up(1);
             Some(EventResult::Consumed(None))
-        }).on_pre_event_inner('j', |s| {
+        }).on_pre_event_inner('j', |s, _| {
             s.select_down(1);
             Some(EventResult::Consumed(None))
         });
